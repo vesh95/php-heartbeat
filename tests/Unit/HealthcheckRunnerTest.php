8569ea7bb0e94ddd32527bbbe03bf4937d6844a7
Eduard Larionov <vesh95.17@ya.ru>
2025-06-25 22:55:59 +0500

    Add tests

---
+++ tests/Unit/HealthcheckRunnerTest.php
@@ -0,0 +1,94 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Vesh95\Pulse\CheckInterface;
+use Vesh95\Pulse\DefaultHealthcheckRunner;
+use Vesh95\Pulse\HealthcheckRunnerInterface;
+use Vesh95\Pulse\Result;
+
+class HealthcheckRunnerTest extends TestCase
+{
+    private int $checksCounter = 1;
+    private readonly HealthcheckRunnerInterface $healthcheckRunner;
+
+    protected function setUp(): void
+    {
+        $this->checksCounter = 1;
+        $this->healthcheckRunner = new DefaultHealthcheckRunner();
+    }
+
+    /**
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
+    public function testOnceSuccess(): void
+    {
+        $runner = $this->healthcheckRunner;
+        $runner->append($this->makeCheck(true));
+
+        $result = $runner->run();
+        $this->assertTrue($result->allOk());
+    }
+
+    /**
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
+    public function testWithOnceFail(): void
+    {
+        $runner = $this->healthcheckRunner;
+        $runner->append($this->makeCheck(false));
+
+        $result = $runner->run();
+        $this->assertFalse($result->allOk());
+    }
+
+    /**
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
+    public function testWithManySuccess(): void
+    {
+        $runner = $this->healthcheckRunner;
+        $runner->append($this->makeCheck(true));
+        $runner->append($this->makeCheck(true));
+
+        $result = $runner->run();
+        $this->assertTrue($result->allOk());
+    }
+
+    /**
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
+    public function testWithManyFailed(): void
+    {
+        $runner = $this->healthcheckRunner;
+        $runner->append($this->makeCheck(false));
+        $runner->append($this->makeCheck(false));
+
+        $result = $runner->run();
+        $this->assertFalse($result->allOk());
+    }
+
+    /**
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
+    public function testWithManySuccessAndFailed(): void
+    {
+        $runner = $this->healthcheckRunner;
+        $runner->append($this->makeCheck(true));
+        $runner->append($this->makeCheck(false));
+        $runner->append($this->makeCheck(true));
+
+        $result = $runner->run();
+        $this->assertFalse($result->allOk());
+    }
+
+    /**
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
+    private function makeCheck(bool $ok): CheckInterface
+    {
+        $check = $this->createStub(CheckInterface::class);
+        $check->method('check')->willReturn(new Result('test_' . $this->checksCounter, $ok, null));
+
+        return $check;
+    }
+}
---
+++ tests/Unit/ResultCollectionTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Vesh95\Pulse\Result;
+use Vesh95\Pulse\ResultsCollection;
+
+class ResultCollectionTest extends TestCase
+{
+    public function testAdd(): void
+    {
+        $result = new Result('test', true, null);
+
+        $collection = new ResultsCollection();
+        $collection->add($result);
+        $this->assertCount(1, $collection);
+    }
+
+    public function testAddRepeat(): void
+    {
+        $result = new Result('test', true, null);
+
+        $collection = new ResultsCollection();
+        $collection->add($result);
+        $collection->add($result);
+        $this->assertCount(1, $collection);
+    }
+
+    public function testAddMultiple(): void
+    {
+        $result1 = new Result('test1', true, null);
+        $result2 = new Result('test2', true, null);
+
+        $collection = new ResultsCollection();
+        $collection->add($result1);
+        $collection->add($result2);
+        $this->assertCount(2, $collection);
+    }
+}
